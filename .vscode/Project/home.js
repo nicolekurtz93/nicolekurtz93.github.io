fb7cd4de18bc5abaa21115acf48d84985deaf0c7
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-03-12 18:30:39 -0700

    added form, cleaned up css

---
+++ .vscode/Project/home.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmission);
+
+function handleSubmission(evt) {
+  const { name, email, tel } = form.elements;
+  var message = ["Please resolve the following errors:\n"];
+  if (!name.value) message.push("No name was provided\n");
+  if (!email.value) message.push("No email was provided\n");
+  if (!tel.value) message.push("No phone number was provided");
+
+  if (message.length > 1) {
+    evt.preventDefault();
+    alert(message.join("-"));
+  } else
+    alert(
+      "Thank you for contacting me! I return messages with 3 business days."
+    );
+}
