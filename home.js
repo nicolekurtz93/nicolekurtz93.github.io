234d02883ad3a6dae0a8ad5ba08a8f951170050b
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-03-13 21:21:24 -0700

    used cat api to add fun to footer

--- home.js
+++ home.js
@@ -1,5 +1,9 @@
+let catLocation = document.querySelector("footer");
 const form = document.querySelector("#form");
+const url = "https://aws.random.cat/meow?ref=apilist.fun";
+
 form.addEventListener("submit", handleSubmission);
+
 function handleSubmission(evt) {
   const { name, email, tel } = form.elements;
   var message = ["Please resolve the following errors:\n"];
@@ -15,3 +19,25 @@ function handleSubmission(evt) {
       "Thank you for contacting me! I return messages with 3 business days."
     );
 }
+
+const fetchData = (url) => {
+  // Add your code here
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let element = document.createElement("img");
+      element.src = data.file;
+      element.style.maxHeight = "100px";
+      catLocation.style.display = "flex";
+      catLocation.style.justifyContent = "center";
+      let text = document.createElement("h2");
+      text.textContent = "Thanks for visiting my site!";
+
+      catLocation.appendChild(element);
+      catLocation.appendChild(text);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+fetchData(url);
